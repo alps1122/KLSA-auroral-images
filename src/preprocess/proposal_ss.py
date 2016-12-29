2849d1662fbf541c786ec9dd1bf0b9b07f33291b
niuchuang <niuchuangnn@gmail.com>
2016-12-29 22:50:11 +0800

    selective search region

--- src/preprocess/proposal_ss.py
+++ src/preprocess/proposal_ss.py
@@ -34,17 +34,17 @@ if __name__=="__main__":
     # parser.add_argument('-a', '--alpha',    type=float, default=1.0, help='alpha value for compositing result image with input image')
     # args = parser.parse_args()
 
-    imgFile = '../../Data/labeled2003_38044/N20031223G120622.bmp'
+    imgFile = '/home/niuchuang/data/AuroraData/Aurora_img/3/N20031221G101551.jpg'
     k = 100
     feature_masks = [1, 1, 1, 1]  # ['size', 'color', 'texture', 'fill']
     out_prefix = ''
     alpha = 0.5  # alpha value for compositing result image with input image
 
-    img = skimage.io.imread(imgFile)
-    if len(img.shape) == 2:
-        img = skimage.color.gray2rgb(img)
+    im = skimage.io.imread(imgFile)
+    if len(im.shape) == 2:
+        img = skimage.color.gray2rgb(im)
 
-    (R, F, L) = selective_search.hierarchical_segmentation(img, k, feature_masks, eraseMap=None)
+    (R, F, L, L_regions) = selective_search.hierarchical_segmentation(img, k, feature_masks, eraseMap=None)
     print('result filename: %s_[0000-%04d].png' % (out_prefix, len(F) - 1))
 
     # suppress warning when saving result images
@@ -61,13 +61,24 @@ if __name__=="__main__":
 
     print('\n')
 
-    map = np.zeros((440, 440))
+    erase_map = np.zeros((440, 440))
     centers = np.array([219.5, 219.5])
     for i in range(440):
         for j in range(440):
             if np.linalg.norm(np.array([i, j]) - centers) > 220+5:
-                map[i, j] = 1
-    (R, F, L) = selective_search.hierarchical_segmentation(img, k, feature_masks, eraseMap=map)
+                erase_map[i, j] = 1
+    (R, F, L, L_regions) = selective_search.hierarchical_segmentation(img, k, feature_masks, eraseMap=erase_map)
+
+    # region_img = F[0]
+    # for i in range(60,80):
+    #     grid = [L[i]]
+    #     sub_regions = L_regions[i]
+    #     im_psudo = np.zeros((440, 440))
+    #     for l in sub_regions:
+    #         im_psudo[np.where(region_img == l)] = 255
+    #     plf.showGrid(im_psudo, grid)
+    # plt.show()
+
     out_prefix = 'erase'
     print('result filename: %s_[0000-%04d].png' % (out_prefix, len(F) - 1))
 
@@ -86,14 +97,52 @@ if __name__=="__main__":
     print('\n')
 
     color_space = ['rgb']
-    ks = [100]
-    regions = selective_search.selective_search(img, color_spaces=color_space, ks=ks, feature_masks=feature_masks)
+    ks = [50, 100, 200]
+    region_set = selective_search.selective_search(img, color_spaces=color_space, ks=ks, feature_masks=feature_masks, eraseMap=erase_map)
 
-    grids = [x[1] for x in regions]
-    plf.showGrid(img, grids)
+    # region_labels = []
+    # for i in range(len(region_set)):
+    #     region_labels.append(region_set[i][-1])
+    #     region_set[i] = region_set[i][0]
 
-    # plt.figure(2)
+    # grids = [x[1] for x in regions]
+    proposal_minSize = 100 * 100
+    proposal_maxSize = 440 * 220
 
-    # plt.imshow(map, cmap='gray')
-    plt.show()
+    # regions = []
+    for rs in region_set:
+        ri = rs[0]
+        I = rs[1]
+        eraseLabels = set(list(I[numpy.where(erase_map == 1)].flatten()))
+        regions = []
+        for r in ri:
+            exist_eraseLabels = [l for l in eraseLabels if l in r[2]]
+            if len(exist_eraseLabels) == 0:
+                grid_axes = r[1]
+                h = grid_axes[2] - grid_axes[0]
+                w = grid_axes[3] - grid_axes[1]
+                if (h*w >= proposal_minSize) and (h*w <= proposal_maxSize):
+                    regions.append(r)
+
+        # regions = [x[0] for x in region_set]
+        # region_imgs = [x[-1] for x in region_set]
+
+        # proposals = sorted(regions[0])
+        # region_img = region_imgs[0]
+        proposals = regions
+        region_img = I
+
+        for i in range(len(regions)):
+            cornors = proposals[i][1]
+            grid = [[cornors[0], cornors[1], cornors[2] - cornors[0] + 1, cornors[3] - cornors[1] + 1]]
+            sub_regions = proposals[i][-1]
+            im_psudo = np.zeros((440, 440))
+            for l in sub_regions:
+                im_psudo[np.where(region_img==l)] = im[np.where(region_img==l)]
+            plf.showGrid(im_psudo, grid)
+
+        # plt.figure(2)
+
+        # plt.imshow(map, cmap='gray')
+        plt.show()
     pass
