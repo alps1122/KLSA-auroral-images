935e5e17e2210b7176f1aac041a5d6e605ce0ab1
niuchuang <niuchuangnn@gmail.com>
2017-01-07 20:51:59 +0800

    update paseLabeldFile function for training CNN

--- src/util/paseLabeledFile.py
+++ src/util/paseLabeledFile.py
@@ -1,5 +1,7 @@
 import datetime
 import matplotlib.pyplot as plt
+from scipy.misc import imread
+import numpy as np
 
 def parseNL(path):
     f = open(path, 'r')
@@ -53,6 +55,17 @@ def sampleImages(names, mode='Uniform', timediff = 60, sampleNum = 500):
             # ids.append(idx[i])
         return names[:sampleNum]
 
+def writeArrangeImgsToFile(arrangeImgs, filePath, labelAdjust=None, addType=None):
+    f = open(filePath, 'w')
+    for label, images in arrangeImgs.iteritems():
+        if labelAdjust is not None:
+            label = str(int(label) + labelAdjust)
+        for name in images:
+            if addType is not None:
+                name += addType
+            f.write(name + ' ' + label + '\n')
+    f.close()
+    return 0
 
 def arrangeToClasses(names, labels, classNum=4, classLabel=[['1'], ['2'], ['3'], ['4']]):
     arrangeImgs = {}
@@ -71,18 +84,66 @@ def arrangeToClasses(names, labels, classNum=4, classLabel=[['1'], ['2'], ['3'],
     if classNum < 4:
         return arrangeImgs, rawTypes
 
+def arrangeToDays(names, labels):
+    name_days = {}
+    label_days = {}
+    days = []
+    names_num = len(names)
+    for i in xrange(names_num):
+        name = names[i]
+        label = labels[i]
+        day = name[1:9]
+
+        if day not in name_days:
+            name_days[day] = []
+            label_days[day] = []
+            days.append(day)
+        name_days[day].append(name)
+        label_days[day].append(label)
+    return name_days, label_days, days
+
+def splitConfigFile(sourceFile, savePathes=['../../Data/train_day16.txt', '../../Data/test_day3.txt'],
+                    splitGroups=[range(15), range(15, 19)], isBalabceSamples=[True, False], labelAdjust=None, addType=None):
+    [names, labels] = parseNL(sourceFile)
+    names_days, labels_days, days = arrangeToDays(names, labels)
+
+    groups_num = len(splitGroups)
+    for i in xrange(groups_num):
+        group_i = splitGroups[i]
+        savePath_i = savePathes[i]
+        isBalance_i = isBalabceSamples[i]
+        group_i_dayNum = len(group_i)
+        names_i = []
+        labels_i = []
+        for j in xrange(group_i_dayNum):
+            names_i += names_days[days[group_i[j]]]
+            labels_i += labels_days[days[group_i[j]]]
+        arrangeImgs_i = arrangeToClasses(names_i, labels_i, classNum=4, classLabel=[['1'], ['2'], ['3'], ['4']])
+        if isBalance_i:
+            max_balance_num = min([len(x) for x in arrangeImgs_i.values()])
+            arrangeImgs_i = balanceSample(arrangeImgs_i, max_balance_num)
+        writeArrangeImgsToFile(arrangeImgs_i, savePath_i, labelAdjust, addType)
+    return 0
+
 def balanceSample(arrangedImgs, sampleNum):
     for c in arrangedImgs:
         arrangedImgs[c] = sampleImages(arrangedImgs[c], mode='random', sampleNum=sampleNum)
     return arrangedImgs
 
-def compareLabeledFile(file_std, file_compare):
+def compareLabeledFile(file_std, file_compare, labelAdjust=None, addType=None):
     [names_std, labels_std] = parseNL(file_std)
     [names_compare, labels_compare] = parseNL(file_compare)
     flag = True
     for i in range(len(names_compare)):
-        std_idx = names_std.index(names_compare[i])
-        if labels_std[std_idx] != labels_compare[i]:
+        name_c = names_compare[i]
+        if addType is not None:
+            name_c = name_c[:-len(addType)]
+        std_idx = names_std.index(name_c)
+        label_c = labels_compare[i]
+        if labelAdjust is not None:
+            label_c = str(int(label_c)+labelAdjust)
+        label_std = labels_std[std_idx]
+        if label_std != label_c:
             flag = False
             break
     return  flag
@@ -122,3 +183,48 @@ def showGrid(im, gridList):
     plt.axis('off')
     plt.tight_layout()
     plt.draw()
+
+def calculateImgsMean(imgsFile, dataFolder, imgType=None):
+    [names, labels] = parseNL(imgsFile)
+    mean = 0.
+    for name in names:
+        if imgType is not None:
+            imgFile = dataFolder + name + imgType
+        else:
+            imgFile = dataFolder + name
+        im = imread(imgFile)
+        mean += im.mean()
+    mean = mean / len(names)
+    print imgsFile + ' mean: ' + str(mean)
+    return mean
+def adjustLables(labelFile, adjust=-1):
+    [names, labels] = parseNL(labelFile)
+    labels_num = [int(x) for x in labels]
+    labels_num = list(np.array(labels_num) + adjust)
+    labels = [str(x) for x in labels_num]
+    f = open(labelFile, 'w')
+    for i in xrange(len(labels)):
+        name = names[i]
+        label = labels[i]
+        f.write(name + ' ' + label + '\n')
+    f.close()
+    return 0
+
+if __name__ == '__main__':
+    # allLabels = '../../Data/Alllabel2003_38044.txt'
+    # savePathes = ['../../Data/train_day16_all.txt', '../../Data/test_day3_all.txt']
+    # imgType = '.jpg'
+    # # days = ['20031221', '20031222', '20031223', '20031224', '20031225', '20031226', '20031227', '20031228', '20031229',
+    # #  '20031230', '20031231', '20040101', '20040102', '20040103', '20040112', '20040114', '20040116', '20040117',
+    # #  '20040118']
+    # [names, labels] = parseNL(allLabels)
+    #
+    # names_days, labels_days, days = arrangeToDays(names, labels)
+    # splitConfigFile(allLabels, savePathes=savePathes, labelAdjust=-1, addType=imgType, isBalabceSamples=[False, False])
+    # # calculateImgsMean(savePathes[0], dataFolder='../../Data/all38044JPG/')
+    # print compareLabeledFile(allLabels, savePathes[0], labelAdjust=1, addType=imgType)
+    # print compareLabeledFile(allLabels, savePathes[1], labelAdjust=1, addType=imgType)
+    # print days
+    labelFile1 = '../../Data/train_16_3_7_2663.txt'
+    adjustLables(labelFile1)
+    pass
